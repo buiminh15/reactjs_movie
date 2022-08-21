2de8a459ab3afbc356b037e1700a86c0427d19c8
buiminh15 <buiminh15@gmail.com>
2022-08-22 00:56:55 +0700

    feat: using react-hook-form to handle validation in form

--- src/components/layout/Auth.js
+++ src/components/layout/Auth.js
@@ -2,22 +2,58 @@ import React from 'react';
 import { Row } from '../common';
 import { Input } from '../styles';
 import { FaRegUserCircle } from 'react-icons/fa';
-import { FiMail } from "react-icons/fi";
-import { RiLockPasswordLine } from "react-icons/ri";
+import { FiMail } from 'react-icons/fi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required()
+  })
+  .required();
 
 const SignIn = () => {
   return <div>Sign in</div>;
 };
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Row>
-        <Input label='First name' icon={<FaRegUserCircle />} />
-        <Input label='Last name' icon={<FiMail />} />
-        <Input label='First name' icon={<RiLockPasswordLine />} />
+        <Input
+          label="First name"
+          name="firstName"
+          {...register('firstName')}
+          icon={<FaRegUserCircle />}
+          errors={errors}
+        />
+        <Input
+          label="Last name"
+          name="lastName"
+          {...register('lastName')}
+          icon={<FaRegUserCircle />}
+        />
+        <Input label="Email" name="email" {...register('email')} icon={<FiMail />} />
+        <Input
+          label="Password"
+          name="password"
+          {...register('password')}
+          icon={<RiLockPasswordLine />}
+        />
       </Row>
-    </div>
+      <input type="submit" value="submit" />
+    </form>
   );
 };
 
--- src/components/styles/Input.js
+++ src/components/styles/Input.js
@@ -1,27 +1,29 @@
 import { CustomIcon } from '../common';
 
-const Input = ({ label, name, type = 'text', icon,  ...rest }) => {
+const Input = ({ label, name, type = 'text', icon, errors, ...rest }) => {
+  console.log('ERRORS::: ', name && errors?.[name]);
   return (
     <div className="relative text-white">
-      <input
-        {...rest}
-        id={name}
-        type={type}
-        placeholder=" "
-        className="block w-full appearance-none rounded border border-gray-500 bg-transparent px-4 py-2 outline-none "
-      />
-      <CustomIcon
-        providerStyle={{
-          className: 'absolute right-4 text-2xl top-1/2 -translate-y-1/2 clr-gray-500'
-        }}
-        element={icon}
-      />
-      <label
-        htmlFor={name}
-        className="absolute-y-center bg-primary clr-gray-500 pointer-events-none left-2 select-none px-2 duration-300"
-      >
-        {label}
-      </label>
+      <div>
+        <input
+          {...rest}
+          id={name}
+          type={type}
+          placeholder=" "
+          className="block w-full appearance-none rounded border border-gray-500 bg-transparent px-4 py-2 outline-none "
+        />
+        <CustomIcon
+          providerStyle={{
+            className: 'absolute right-4 text-2xl top-1/2 -translate-y-1/2 clr-gray-500'
+          }}
+          element={icon}
+        />
+        <label
+          htmlFor={name}
+          className="absolute-y-center bg-primary clr-gray-500 pointer-events-none left-2 select-none px-2 duration-300">
+          {label}
+        </label>
+      </div>
     </div>
   );
 };
